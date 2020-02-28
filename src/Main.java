66cd1e26485e8f7ab21b66dd19b0b376b74d7e68
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-28 21:10:39 +0200

    Some improvements

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args)throws FileNotFoundException {
-        char[][] labirint = ReadWrite.read_file("/Users/nikitasakun/Desktop/Java project/Lab_2_Prog/Test/file_labirint");
+        char[][] labirint = ReadWrite.read_file("Test/file_labirint");
         ReadWrite.out_labirint(labirint);
     }
 }
