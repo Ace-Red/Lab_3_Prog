4a167e651d06eca60f3c0f07a709f418855d1128
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-28 19:21:09 +0200

    I fixed my bugs

--- src/AStar.java
+++ src/AStar.java
@@ -14,30 +14,32 @@ public class AStar {
         while (h(path[numberOfPath-1], finish)!=0) {
             int[][] points = new int[4][3];
             int iHavePoints = 0;
-            int[][] suspect = new int[4][2];
-            suspect[0][0] = points[0][0] + 1;
-            suspect[1][0] = points[0][0] - 1;
-            suspect[0][1] = suspect[1][1] = points[0][1];
+            int[][] suspect = new int[4][3];
+            suspect[0][0] = path[numberOfPath-1][0] + 1;
+            suspect[1][0] = path[numberOfPath-1][0] - 1;
+            suspect[0][1] = suspect[1][1] = path[numberOfPath-1][1];
 
-            suspect[2][1] = points[0][1] + 1;
-            suspect[3][1] = points[0][1] - 1;
-            suspect[2][0] = suspect[3][0] = points[0][0];
+            suspect[2][1] = path[numberOfPath-1][1] + 1;
+            suspect[3][1] = path[numberOfPath-1][1] - 1;
+            suspect[2][0] = suspect[3][0] = path[numberOfPath-1][0];
 
             for (int k = 0; k < 4; k++) {
-                if (isWall(field, suspect[k])) {
+                if (!isWall(field, suspect[k])) {
                     points[iHavePoints] = suspect[k];
-                    points[iHavePoints][2] = h(path[numberOfPath - 1], suspect[k]) + numberOfPath;
+                    points[iHavePoints][2] =h(finish, suspect[k]) + numberOfPath;
                     iHavePoints++;
                 }
             }
-            path[numberOfPath]=minimum(points, iHavePoints);
+            int[] minimum =minimum(points, iHavePoints);
+            path[numberOfPath]=minimum;
+            close(field,minimum, numberOfPath);
             numberOfPath++;
         }
         return path;
     }
 
     private static boolean isWall(char[][] field, int[] coordinates) {
-        return field[coordinates[0]][coordinates[1]] == 'X';
+        return field[coordinates[0]][coordinates[1]] != ' ';
     }
 
     private static int[] minimum(int[][] points, int iHavePoints) {
@@ -49,5 +51,9 @@ public class AStar {
         int[] array = new int[2];
         System.arraycopy(points[minimum], 0, array, 0, 2);
         return array;
+    }
+
+    private static void close(char[][] field, int[]point,int number){
+        field[point[0]][point[1]]=(char)('0'+number);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args)throws FileNotFoundException {
-        char[][] labirint = ReadWrite.read_file("/Users/nikitasakun/Desktop/Java project/Lab_2_Prog/Test/file_labirint");
-        ReadWrite.out_labirint(labirint);
+    public static void main(String[] args) throws FileNotFoundException {
+        char[][] maze = ReadWrite.read_file("Test/file_labirint");
+        ReadWrite.out_labirint(maze);
+        int[] finish = new int[2];
+        finish[0] = 1;
+        finish[1] = 6;
+        System.out.println(Arrays.deepToString(AStar.findPath(maze, 6, 1, finish)));
     }
 }
