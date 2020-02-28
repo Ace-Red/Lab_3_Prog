1724b38ee2971320e0610b95bae1783fc0db3029
Nikita Sakun <NikitaSakun1@gmail.com>
2020-02-28 21:07:41 +0200

    I create Queue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args)throws FileNotFoundException {
         char[][] labirint = ReadWrite.read_file("/Users/nikitasakun/Desktop/Java project/Lab_2_Prog/Test/file_labirint");
         ReadWrite.out_labirint(labirint);
-        ArrayList<Character> arrayList = new ArrayList<>();
     }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,8 +1,28 @@
+
 import java.util.ArrayList;
 
 public class Queue {
+    ArrayList<Character> queue;
+    private int len;
 
-    public static char[] AddQueue(char elem){
-
+    public Queue(){
+        queue = new ArrayList<>();
+        len = 0;
+    }
+    public void add(char element){
+        queue.add(element);
+        len++;
+    }
+    public void remove(){
+        queue.remove(0);
+        len--;
+    }
+    public int getLen(){
+        return len;
+    }
+    public void outQueue(){
+        for(int i = 0;i<len;i++){
+            System.out.println(queue);
+        }
     }
 }
