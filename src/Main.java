8b7e394a06dbc07fe6c95587f6c7e9ffaf5ed59e
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-28 21:15:12 +0200

    New features

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args)throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
         char[][] labirint = ReadWrite.read_file("Test/file_labirint");
         ReadWrite.out_labirint(labirint);
+        int[] finish = new int[2];
+        finish[0] = 1;
+        finish[1] = 6;
+        System.out.println(Arrays.deepToString(AStar.findPath(labirint, 6, 1, finish)));
     }
 }
