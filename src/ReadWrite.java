2cb41e4d1e183771ffbf1488b2940a443b2af7d8
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-29 21:32:20 +0200

    I improved ReadWrite part

--- src/ReadWrite.java
+++ src/ReadWrite.java
@@ -5,30 +5,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadWrite {
-    public static char[][] read_file(String file_name) throws FileNotFoundException {
+    public static char[][] read_file(String file_name) {
         File info = new File(file_name);
-        Scanner scanner = new Scanner(info);
-        String first = scanner.nextLine();
-        char[][] labirint = new char[(first.length() / 2) + 1][(first.length() / 2) + 1];
-        Arrays.fill(labirint[0], 'X');
-        for (int i = 1; i < labirint.length; i++) {
-            String char_arr = scanner.nextLine();
-            int counter = 0;
-            for (int j = 0; j < char_arr.length(); j += 2) {
-                labirint[i][counter] = char_arr.charAt(j);
-                counter++;
+        Scanner scanner;
+        try {
+            scanner = new Scanner(info);
+            String first = scanner.nextLine();
+            char[][] labirint = new char[(first.length() / 2) + 1][(first.length() / 2) + 1];
+            Arrays.fill(labirint[0], 'X');
+            for (int i = 1; i < labirint.length; i++) {
+                String char_arr = scanner.nextLine();
+                int counter = 0;
+                for (int j = 0; j < char_arr.length(); j += 2) {
+                    labirint[i][counter] = char_arr.charAt(j);
+                    counter++;
+                }
             }
+            return labirint;
+        } catch (FileNotFoundException ex) {
+            System.out.println("Your path is incorrect!\n" + ex);
+            return new char[0][0];
         }
-        return labirint;
+
+
     }
 
     public static void out_labirint(char[][] matrix, String file_name) throws FileNotFoundException {
         File file = new File(file_name);
         PrintWriter pw = new PrintWriter(file);
         pw.println("Вот ваш конечный лабиринт: ");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                pw.printf(" %c", matrix[i][j]);
+        for (char[] chars : matrix) {
+            for (char aChar : chars) {
+                pw.printf(" %c", aChar);
             }
             pw.println();
         }
